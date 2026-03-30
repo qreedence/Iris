a90bb21ecdd88ee61e37a18e03922996ac89a5f2
qreedence <eden.yusof.ioannidis@gmail.com>
2026-03-30 22:22:33 +0200

    scaffold

---
+++ src/Iris.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Iris.Infrastructure.Persistence;
+
+namespace Iris.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
