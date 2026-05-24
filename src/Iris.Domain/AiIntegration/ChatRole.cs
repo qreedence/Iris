a9f356f62b943dc2cfc56f3a8f8bbe1028e5d94c
qreedence <eden.yusof.ioannidis@gmail.com>
2026-05-24 13:48:03 +0200

    add JsonConverter to ChatRole enum

--- src/Iris.Domain/AiIntegration/ChatRole.cs
+++ src/Iris.Domain/AiIntegration/ChatRole.cs
@@ -1,5 +1,8 @@
-﻿namespace Iris.Domain.AiIntegration
+﻿using System.Text.Json.Serialization;
+
+namespace Iris.Domain.AiIntegration
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum ChatRole
     {
         System,
